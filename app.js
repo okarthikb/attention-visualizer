1b43bbc235ec924d657f915fa9d90d4148a0862f
okarthikb <86470305+okarthikb@users.noreply.github.com>
2023-05-16 04:38:51 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const promptForm = document.getElementById('prompt-form');
+  const promptTextElement = document.getElementById('prompt-text');
+  const layerSlider = document.getElementById('layer');
+  const headSlider = document.getElementById('head');
+  const layerValue = document.getElementById('layer-value');
+  const headValue = document.getElementById('head-value');
+  let spans = [];
+  let attentions = [];
+
+  function visualizeAttention(currentIndex) {
+    const layer = parseInt(layerSlider.value) - 1;
+    const head = parseInt(headSlider.value) - 1;
+    const attentionScores = attentions[layer][head][currentIndex];
+    spans.forEach((span, i) => span.style.backgroundColor = `rgba(255, 255, 0, ${attentionScores[i]})`);
+  }
+
+  [layerSlider, headSlider].forEach(slider => {
+    slider.addEventListener('input', function() {
+      const highlightedElement = document.querySelector('.highlight');
+      if (highlightedElement) {
+        const currentIndex = parseInt(highlightedElement.dataset.index);
+        visualizeAttention(currentIndex);
+      }
+      slider.id === 'layer' ? layerValue.textContent = slider.value : headValue.textContent = slider.value;
+    });
+  });
+
+  promptForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const prompt = document.getElementById('prompt').value;
+    const data = {
+      prompt: prompt
+    };
+    fetch('/attention_scores', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => {
+        const inputTokens = data.input_tokens;
+        attentions = data.attentions;
+        spans = [];
+        promptTextElement.innerHTML = '';
+
+        inputTokens.forEach((token, i) => {
+          const span = document.createElement('span');
+          span.innerHTML = token.replace(/Ġ/g, ' ').replace(/Ċ/g, '<br>');
+          span.dataset.index = i;
+          span.addEventListener('click', function() {
+            const currentIndex = parseInt(this.dataset.index);
+            spans.forEach((span) => span.style.backgroundColor = 'transparent');
+            if (this.classList.contains('highlight')) {
+              this.classList.remove('highlight');
+            } else {
+              this.classList.add('highlight');
+              visualizeAttention(currentIndex);
+            }
+          });
+
+          spans.push(span);
+          promptTextElement.appendChild(span);
+        });
+      });
+  });
+});
---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, jsonify, send_file
+from transformers import AutoTokenizer, GPT2Model
+import torch
+
+
+app = Flask(__name__, template_folder='./', static_folder='./')
+
+model = GPT2Model.from_pretrained("gpt2").eval().to('cpu')
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+@app.route('/app.js')
+def serve_js():
+  return send_file('app.js', mimetype='application/javascript')
+
+
+@app.route('/cmu.sans-serif-medium.ttf')
+def serve_font():
+  return send_file('cmu.sans-serif-medium.ttf', mimetype='font/ttf')
+
+
+@app.route('/style.css')
+def serve_css():
+  return send_file('style.css', mimetype='text/css')
+
+
+@app.route('/attention_scores', methods=['POST'])
+def attention_scores():
+  prompt = request.json.get('prompt')
+  input_tokens = tokenizer.tokenize(prompt)
+  input_token_ids = tokenizer(prompt, return_tensors="pt").input_ids.to('cpu')
+
+  with torch.no_grad():
+    attentions = model(input_token_ids, output_attentions=True).attentions
+  attentions = [attention.squeeze(0).tolist() for attention in attentions]
+
+  return jsonify({'input_tokens': input_tokens, 'attentions': attentions})
+
+
+if __name__ == '__main__':
+  app.run(host='localhost', port=10000, debug=True)
